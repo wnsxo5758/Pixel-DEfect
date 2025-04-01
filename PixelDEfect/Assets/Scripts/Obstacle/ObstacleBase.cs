6a056461dcb834f780778461c2911415ee7d7e08
workantL <passant321@naver.com>
2025-04-01 23:48:42 +0900

    물 장애물 코드 추가

--- PixelDEfect/Assets/Scripts/Obstacle/ObstacleBase.cs
+++ PixelDEfect/Assets/Scripts/Obstacle/ObstacleBase.cs
@@ -7,9 +7,13 @@ public class ObstacleBase : MonoBehaviour
     [SerializeField]
     private bool isInstantDeath = false; // 즉사인지 확인
     [SerializeField]
+    private bool continuousDamage; // 지속적인 데미지인가?
+    [SerializeField]
     private int damage;
     [SerializeField]
     private bool canDestory = false; // 플레이어와 접촉 후, 파괴되는지
+
+    private Coroutine damageCoroutine;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return; // 대상이 플레이어가 아니라면 return
@@ -19,7 +23,10 @@ public class ObstacleBase : MonoBehaviour
             collision.GetComponent<PlayerHp>().DecreaseHp(damage);
             collision.GetComponent<PlayerInteraction>().MoveToSpawnPoint();
         }
-
+        else if (continuousDamage)
+        {
+            return;
+        }
         else // 즉사 장애물이 아니라면 체력 감소
         {
             collision.GetComponent<PlayerHp>().DecreaseHp(damage);
@@ -27,6 +34,42 @@ public class ObstacleBase : MonoBehaviour
             {
                 StartCoroutine(nameof(ObstacleDestory));
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(continuousDamage)
+        {
+            if (damageCoroutine != null)
+            {
+                damageCoroutine = null;
+                StopAllCoroutines();
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            if(damageCoroutine == null)
+            {
+                damageCoroutine = StartCoroutine(ApplyContinuousDamage(collision.GetComponent<PlayerHp>()));
+            }
+        }
+        else if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<EnemyFSM>().DecreaseHp(damage);
+        }
+    }
+
+    private IEnumerator ApplyContinuousDamage(PlayerHp hp)
+    {
+        while(true)
+        {
+            hp.DecreaseHp(damage);
+            yield return new WaitForSeconds(1f);
         }
     }
 
