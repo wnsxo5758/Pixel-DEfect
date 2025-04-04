fb911bd8b0191e917079a54262539b9fc5cf3f51
workantL <passant321@naver.com>
2025-04-04 21:34:16 +0900

    몬스터 스포너 딜레이 추가

--- PixelDEfect/Assets/Scripts/Spawner.cs
+++ PixelDEfect/Assets/Scripts/Spawner.cs
@@ -7,15 +7,28 @@ public class Spawner : MonoBehaviour
 
     [SerializeField]
     private GameObject spawnEnemy; // 스폰될 wjr
-
+    [SerializeField]
+    private bool isActive;
+    [SerializeField]
+    private float delayTime; // 딜레이 시간
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
+            StartCoroutine(nameof(SpawnDelay));
+        }
+    }
+
+
+    private IEnumerator SpawnDelay()
+    {
+        if(isActive == false)
+        {
+            isActive = true;
+            yield return new WaitForSeconds(delayTime);
             Instantiate(spawnEnemy, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
-
 }
