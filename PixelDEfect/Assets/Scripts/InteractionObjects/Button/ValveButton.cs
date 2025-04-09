55ea8cfe853f0d67d041fbea4a24f5018989da51
workantL <passant321@naver.com>
2025-04-09 21:54:01 +0900

    밸브 회전 제외하고는 완성

--- PixelDEfect/Assets/Scripts/InteractionObjects/Button/ValveButton.cs
+++ PixelDEfect/Assets/Scripts/InteractionObjects/Button/ValveButton.cs
@@ -13,51 +13,45 @@ public class ValveButton : ButtonBase
     private float currentPressTime; // 현재 누른 시간
     public bool isPressing; // 벨브를 누르고 있는가
 
-
+    private Collider2D colllider;
+    private void Start()
+    {
+        colllider = GetComponent<Collider2D>();
+    }
     private void Update()
     {
-        // 요구치에 도달했지만 아직 작동하지 않은 경우
-        if (isActiving == false && currentPressTime >= pressTimeRequired)
+        if(isPressing)
         {
-            isActiving = true;
-            StartCoroutine(nameof(ButtonActive));
+            if (currentPressTime < pressTimeRequired)
+            {
+                Debug.Log($"밸브 작동 중 : {currentPressTime}");
+                currentPressTime += Time.deltaTime;
+            }
+
+            else if(currentPressTime >= pressTimeRequired && isActiving == false)
+            {
+                StartCoroutine(nameof(ButtonActive));
+            }
+            
         }
-        else if (!isPressing)
+        if (!isPressing)
         {
             //밸브가 작동이 완료된 상태가 아니고, 누른 시간이 0보다 큰 경우
             if (currentPressTime > 0 && isActiving == false)
             {
                 //시간이 지날수록 누른 시간이 떨어진다.
                 currentPressTime -= Time.deltaTime;
+                Debug.Log($"밸브 떨어지는 중 : {currentPressTime}");
             }
         }
     }
     
-    public void Pressing()
-    {
-        StartCoroutine(nameof(PullingValve));
-    }
-    private void NotPressing()
-    {
-        StopCoroutine(nameof(PullingValve));
-    }
-    protected IEnumerator PullingValve()
-    {
-        if(isPressing == false)
-        {
-            isPressing = true;
-            if (currentPressTime < pressTimeRequired)
-                currentPressTime += Time.deltaTime;
-            else isPressing = false;
-            yield return null;
-        }
-
-    }
     
     protected override IEnumerator ButtonActive() // 버튼을  누른경우
     {
         isActiving = true; // 작동시작
         audio.Play();  // 효과음 
+        colllider.enabled = false;
         if (connectedObjects != null) // 작동되는 오브젝트가 있다면
         {
             foreach (var obj in connectedObjects)
