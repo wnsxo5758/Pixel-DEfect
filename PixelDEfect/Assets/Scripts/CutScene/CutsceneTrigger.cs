391bf840e1ec21fb3e921d7c2507766fa111f572
wnsxo5758 <wnsxo5758@gmail.com>
2025-04-13 00:21:02 +0900

    에셋 수정

--- PixelDEfect/Assets/Scripts/CutScene/CutsceneTrigger.cs
+++ PixelDEfect/Assets/Scripts/CutScene/CutsceneTrigger.cs
@@ -6,10 +6,54 @@ public class CutsceneTrigger : MonoBehaviour
 {
     public PlayableDirector timelineDirector;
 
-    public CinemachineVirtualCamera playerVCam;   // 새로 만든 플레이어 따라가는 VCam
+    public CinemachineVirtualCamera playerVCam;     // 새로 만든 플레이어 따라가는 VCam
     public CinemachineVirtualCamera[] cutsceneVCams;
 
     private bool hasPlayed = false;
+
+    public GameObject playerObject;                 // 플레이어 오브젝트 할당
+
+    private Vector2 originalVelocity;
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+    RuntimeAnimatorController originalAnimator;
+
+    public void DisablePlayerControl()
+    {
+        playerRb = playerObject.GetComponent<Rigidbody2D>();
+        playerAnimator = playerObject.GetComponent<Animator>();
+
+        if (playerRb != null)
+        {
+            originalVelocity = playerRb.velocity;
+            playerRb.velocity = Vector2.zero;
+            playerRb.simulated = false;
+        }
+
+        if (playerAnimator != null)
+        {
+            originalAnimator = playerAnimator.runtimeAnimatorController;
+            playerAnimator.runtimeAnimatorController = null; // 애니메이션 완전 정지
+        }
+
+        playerObject.GetComponent<Collider2D>().enabled = false;
+    }
+
+    public void EnablePlayerControl()
+    {
+        if (playerRb != null)
+        {
+            playerRb.simulated = true;
+            playerRb.velocity = originalVelocity;
+        }
+
+        if (playerAnimator != null && originalAnimator != null)
+        {
+            playerAnimator.runtimeAnimatorController = originalAnimator; // 애니메이터 복구
+        }
+
+        playerObject.GetComponent<Collider2D>().enabled = true;
+    }
 
     private void OnEnable()
     {
@@ -28,6 +72,7 @@ public class CutsceneTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             hasPlayed = true;
+            DisablePlayerControl();
 
             // 컷씬 카메라 우선순위 높임
             foreach (var cam in cutsceneVCams)
@@ -46,6 +91,7 @@ public class CutsceneTrigger : MonoBehaviour
 
         foreach (var cam in cutsceneVCams)
             cam.Priority = 10;
+        EnablePlayerControl();
     }
 
     public void ForcePlay()
@@ -53,6 +99,7 @@ public class CutsceneTrigger : MonoBehaviour
         if (hasPlayed) return;
 
         hasPlayed = true;
+        DisablePlayerControl();
 
         foreach (var cam in cutsceneVCams)
             cam.Priority = 20;
