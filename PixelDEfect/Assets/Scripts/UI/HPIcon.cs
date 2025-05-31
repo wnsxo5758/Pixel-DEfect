264355f78918f2212ccaf0ab78e433e9453d36d6
workantL <passant321@naver.com>
2025-06-01 02:28:43 +0900

    HPUI 추가

---
+++ PixelDEfect/Assets/Scripts/UI/HPIcon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HPIcon : MonoBehaviour
+{
+    [Header("현재 체력")]
+    [SerializeField]
+    private int currentHP;
+
+
+    Animator animator;
+    private void Awake()
+    {
+
+        animator = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        SetState(currentHP); 
+    }
+
+    public void SetState(int damage) 
+    {
+        currentHP -= damage;
+        SetHPAnim(currentHP);
+    }
+
+    private void SetHPAnim(int _currentHP)
+    {
+        animator.SetInteger("currentHP", _currentHP);
+    }
+}
